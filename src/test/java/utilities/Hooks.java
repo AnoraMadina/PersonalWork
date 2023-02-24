1b93e9e3669f60e8e84174400aa5646e6288319e
MadinaMova <inogomovadina@gmail.com>
2023-02-24 14:48:58 -0500

    utilites

--- src/test/java/utilities/Hooks.java
+++ src/test/java/utilities/Hooks.java
@@ -1,2 +1,32 @@
-package utilities;public class Hooks {
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Hooks {
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+
+    @Before
+    public void setUp() throws InterruptedException{
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options= new ChromeOptions();
+        options.setHeadless(false);
+
+        driver=new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        driver.manage().window().maximize();
+    }
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
 }
